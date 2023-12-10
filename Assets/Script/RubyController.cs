cbdac65247230c322114a8c3987997ae4146670d
MinecraftDawn <eric4902077@gmail.com>
2023-12-11 07:45:23 +0800

    Fix Ruby's Jittering

--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyController : MonoBehaviour
-{
+public class RubyController : MonoBehaviour {
+    private Rigidbody2D rigidbody2D;
+    float horizontal; 
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         // QualitySettings.vSyncCount = 0;
         // Application.targetFrameRate = 10;
     }
@@ -14,12 +18,18 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        
+    }
+
+    private void FixedUpdate() {
         Debug.Log(horizontal + " " + vertical);
         Vector2 position = transform.position;
         position.x += 3f * horizontal * Time.deltaTime;
         position.y += 3f * vertical * Time.deltaTime; 
-        transform.position = position;
+        
+        rigidbody2D.MovePosition(position);
     }
 }
